e0ac2010d00fe789bb2f2a5dc08331d3fc0e409a
sinperknight@gmail.com <QI5K6=NTZZgFMdKU>
2017-12-23 22:25:10 -0600

    update

---
+++ projects/Udemy/The_Web_Developer_Bootcamp/Javascript/Script.js
@@ -0,0 +1,8 @@
+var userName = prompt("What is your name?");
+alert("Nice to meet you, " + userName);
+console.log("Also great to meet you, "+ userName);
+var userFristName = prompt("What is your first name?");
+var userLastName = prompt("What is your last name?");
+var userAge = prompt("What is your age?");
+
+console.log("hello, "+ userFristName +" "+ userLastName +" age: "+ userAge);
