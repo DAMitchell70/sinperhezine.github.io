b02e30b8f4051ea241dc89e1f8eb97e3b07b639c
sinper <Sinperknight@gmail.com>
2018-01-02 13:43:47 -0600

    update

--- projects/Udemy/The_Web_Developer_Bootcamp/JQuery/TodoApp/assets/js/todo.js
+++ projects/Udemy/The_Web_Developer_Bootcamp/JQuery/TodoApp/assets/js/todo.js
@@ -5,10 +5,10 @@ $("ul").on("click", "li", function () {
 
 //click on x to delete todo
 $("ul").on("click", "span", function(){
+	event.stopPropagation();
 	$(this).parent().fadeOut(500,function(){
 		$(this).remove();
 	});
-	event.stopPropagation();
 });
 
 $("input[type='text']").keypress(function(event){
@@ -18,7 +18,11 @@ $("input[type='text']").keypress(function(event){
 	  output = $(this).val();
 	  $(this).val("");
   	  //create a new li add to ul
-	  $("ul").append("<li><span>X</span> "+ output +"</li>");
+	  $("ul").append("<li><span><i class='fa fa-trash' aria-hidden='true'></i></span> "+ output +"</li>");
   }
 	
+});
+
+$(".fa-plus").click(function(){
+	$("input[type='text'").fadeToggle();
 });
