fdaadd5dc3d6cd385a7d445fa127e095b50572bd
sinper <Sinperknight@gmail.com>
2018-01-02 02:47:09 -0600

    update

--- projects/Udemy/The_Web_Developer_Bootcamp/JQuery/TodoApp/assets/js/todo.js
+++ projects/Udemy/The_Web_Developer_Bootcamp/JQuery/TodoApp/assets/js/todo.js
@@ -1,5 +1,24 @@
-$("li").click(function () {
+//check off specific todos by clicking
+$("ul").on("click", "li", function () {
 	$(this).toggleClass("completed");
 });
 
+//click on x to delete todo
+$("ul").on("click", "span", function(){
+	$(this).parent().fadeOut(500,function(){
+		$(this).remove();
+	});
+	event.stopPropagation();
+});
 
+$("input[type='text']").keypress(function(event){
+	var output ="";
+  if(event.which === 13){
+	  //grabbing new todo text from input
+	  output = $(this).val();
+	  $(this).val("");
+  	  //create a new li add to ul
+	  $("ul").append("<li><span>X</span> "+ output +"</li>");
+  }
+	
+});
